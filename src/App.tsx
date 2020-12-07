99592fc73770316da4bad208477bdab617b95b76
Olena Vikariy <olena.vikariy@gmail.com>
2020-12-07 10:56:53 +0800

    add: navbar add: typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+
+import React from 'react';
 import logo from './assets/images/logo_small_rust.svg';
 import './App.css';
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
